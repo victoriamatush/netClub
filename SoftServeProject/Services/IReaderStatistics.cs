380aad7a9ef74a80fdd58e518acf8e63991697e9
RGBoosted <right0346@gmail.com>
2021-12-20 21:58:38 +0200

    Edited IReaderStatistics and added ReaderStatisticsService with full functionality

--- SoftServeProject/Services/IReaderStatistics.cs
+++ SoftServeProject/Services/IReaderStatistics.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace SoftServeProject.Services
 {
-  public interface IReaderStatistics
+    public struct AllReadersStat
+    {
+        public double avgAgeOfClient;
+        public double avgTimeOfWorking;
+        public double avgNumOfRequest;
+    }
+    public struct ReaderStat
+    {
+        public List<Book> books;
+        public double howLongSub;
+    }
+    public interface IReaderStatistics
   {
-    public IActionResult GetReaderStatistics();
-    public IActionResult GetAllReadersStatistics();
-    public IActionResult GetInfoAboutNotReturnedBooks();
+    public ReaderStat GetReaderStatistics(int id);
+    public AllReadersStat GetAllReadersStatistics(DateTime startingDate, DateTime endingDate);
+    public List<int> GetInfoAboutNotReturnedBooks(int id);
 
   }
 }
---
+++ SoftServeProject/Services/ReaderStatisticService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftServeProject.Services
+{
+    
+    public class ReaderStatisticService : IReaderStatistics
+    {
+        UserDBContext db = new UserDBContext();
+        public AllReadersStat GetAllReadersStatistics(DateTime startingDate, DateTime endingDate)
+        {
+            AllReadersStat stat = new AllReadersStat();
+            stat.avgAgeOfClient = db.Readers
+                .Select(s => s.Age)
+                .Average();
+            stat.avgTimeOfWorking = db.Readers
+                .Select(s => Convert.ToInt32(DateTime.Now - s.RegisterDate))
+                .Average();
+            stat.avgNumOfRequest = db.Requests
+                .Where(s => s.DateOfRequest >= startingDate || s.DateOfRequest <= endingDate)
+                .Count();
+            return stat;
+
+        }
+
+        public List<Reader> GetInfoAboutNotReturnedBooks()
+        {
+            List<Reader> users = db.Requests
+                .Where(s => s.DateOfReturning == null && s.IsApproved == true)
+                .Select(s => s.Reader).ToList();
+            return users;
+        }
+
+        public ReaderStat GetReaderStatistics(int id)
+        {
+            ReaderStat stat = new ReaderStat();
+            stat.books = db.Requests
+                .Where(s => s.ReaderId == id || s.IsApproved == true)
+                .Select(s => s.Book)
+                .ToList();
+            stat.howLongSub = db.Readers
+                .Where(s => s.Id == id)
+                .Select(s => Convert.ToInt32(DateTime.Now - s.RegisterDate))
+                .First();
+            return stat;
+        }
+    }
+}
